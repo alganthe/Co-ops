cd9d3d05a6b8dc7cfd9536b07378b23199c856d7
Josuan Albin <alganthe@live.fr>
2016-03-16 11:48:42 +0100

    Add taw view distance script
    Just changed the addaction from "settings" to "taw_vd" for clarity.

--- functions/functions.hpp
+++ functions/functions.hpp
@@ -20,3 +20,25 @@ class derp {
         class AIOccupyBuilding {};
     };
 };
+
+class TAW_VD {
+	tag = "TAWVD";
+
+	class Initialize {
+		file = "functions\misc\taw_vd";
+		class stateTracker {
+			ext = ".fsm";
+			postInit = 1;
+			headerType = -1;
+		};
+
+		class onSliderChanged {};
+		class onTerrainChanged {};
+		class updateViewDistance {};
+		class openMenu {};
+		class onChar {};
+		class openSaveManager {};
+		class onSavePressed {};
+		class onSaveSelectionChanged {};
+	};
+};
