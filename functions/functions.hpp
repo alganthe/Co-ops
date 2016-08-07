ef6b19306a0c834b37eb7567f8a4ba9c8adc0d12
Josuan Albin <alganthe@live.fr>
2016-08-08 01:31:02 +0200

    Remove taw_vd, update diary

--- functions/functions.hpp
+++ functions/functions.hpp
@@ -82,25 +82,3 @@ class derp_vehicleHandler {
         class vehicleSetup {};
     };
 };
-
-class TAW_VD {
-    tag = "TAWVD";
-
-    class Initialize {
-        file = "functions\taw_vd";
-        class stateTracker {
-            ext = ".fsm";
-            postInit = 1;
-            headerType = -1;
-        };
-
-        class onSliderChanged {};
-        class onTerrainChanged {};
-        class updateViewDistance {};
-        class openMenu {};
-        class onChar {};
-        class openSaveManager {};
-        class onSavePressed {};
-        class onSaveSelectionChanged {};
-    };
-};
